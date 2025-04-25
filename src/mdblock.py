41bbfcfa28ca1cd28eb7e939b36ed31b4b9639a3
Atomk <marco.fras@hotmail.it>
2025-04-26 01:36:12 +0200

    feat: recognize markdown block type

--- src/mdblock.py
+++ src/mdblock.py
@@ -1,5 +1,55 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "p"
+    HEADING = "h"
+    CODE = "pre"
+    QUOTE = "blockquote"
+    UNORDERED_LIST = "ul"
+    ORDERED_LIST = "ol"
+
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
     stripped_blocks_map = map(lambda s: s.strip(), blocks)
     return list(filter(lambda text: text != "", stripped_blocks_map))
+
+
+def is_heading(block: str) -> bool:
+    return len(re.findall(r"^#{1,6} ", block)) > 0
+
+
+def _every_line_starts_with(text: str, prefix: str) -> bool:
+    lines = text.splitlines()
+    for line in lines:
+        if not line.strip().startswith(prefix):
+            return False
+    return True
+
+
+def is_ordered_list(block: str):
+    lines = block.splitlines()
+    for line in lines:
+        # Technically each number should increment by one,
+        # but HTML numbers element automatically so we don't care
+        matches = re.findall(r"^\d+\. ", line.strip())
+        if len(matches) == 0:
+            return False
+    return True
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if is_heading(block):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif _every_line_starts_with(block, ">"):
+        return BlockType.QUOTE
+    elif _every_line_starts_with(block, "- "):
+        return BlockType.UNORDERED_LIST
+    elif is_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_mdblock.py
+++ src/test_mdblock.py
@@ -1,6 +1,9 @@
 import unittest
 from mdblock import (
     markdown_to_blocks,
+    is_heading,
+    is_ordered_list,
+    block_to_block_type,
 )
 
 
@@ -41,6 +44,45 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+
+    def test_is_heading(self):
+        self.assertTrue( is_heading("# title"))
+        self.assertTrue( is_heading("## title"))
+        self.assertTrue( is_heading("### title"))
+        self.assertTrue( is_heading("###### title"))
+        self.assertTrue( is_heading("## title ###### title"))
+        self.assertFalse(is_heading("#title"))
+        self.assertFalse(is_heading("###title"))
+        self.assertFalse(is_heading("######title"))
+        # Heading must be at line start
+        self.assertFalse(is_heading("text ## title"))
+        # TODO in most renderers this counts as two blocks
+        self.assertFalse(is_heading("text\n## title"))
+
+
+    def test_is_ordered_list(self):
+        self.assertTrue( is_ordered_list("1. one\n2. two\n3. three"))
+        self.assertTrue( is_ordered_list("1. one 1. one\n2. two 2. two\n3. three 3. three 3. three"))
+        self.assertTrue( is_ordered_list("3. three\n2. two 2. two\n1. one"))
+        self.assertTrue( is_ordered_list("0. zero\n105. one.oh.five"))
+        self.assertTrue( is_ordered_list("000. zerozerozero\n0105. oh-one.oh.five\n1. one"))
+        # space is allowed before list items
+        self.assertTrue( is_ordered_list("1. one\n  2. two\n        3. three"))
+        self.assertTrue( is_ordered_list("    1. one     \n  2. two\n    3. three    "))
+
+        # line not starting with number
+        self.assertFalse(is_ordered_list("one 1. one\ntwo 2. two\n3. three"))
+        self.assertFalse(is_ordered_list("1. one\n2. two\nthree 3. three"))
+        # missing space between separator and text
+        self.assertFalse(is_ordered_list("1.one\n2.two\n3. three"))
+        self.assertFalse(is_ordered_list("1. one\n2.two\n3.three"))
+        # incorrect separator
+        self.assertFalse(is_ordered_list("1, one\n2.two\n3.three"))
+        self.assertFalse(is_ordered_list("1. one\n2.two\n3,three"))
+        # mixed numbering type
+        self.assertFalse(is_ordered_list("a. one\nb.two\n3.three"))
+        self.assertFalse(is_ordered_list("1. one\n2.two\nc.three"))
 
 
 if __name__ == "__main__":
